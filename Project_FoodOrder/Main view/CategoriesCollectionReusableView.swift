1dac066931a3609124a1d105271b4fd1bc0326ea
sangtm97 <minhsangtrieu94@gmail.com>
2022-05-20 13:02:41 -0700

    update source

---
+++ Project_FoodOrder/Main view/CategoriesCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  CategoriesCollectionReusableView.swift
+//  Project_FoodOrder
+//
+//  Created by DeepTM on 20/05/2022.
+//
+
+import UIKit
+
+class CategoriesCollectionReusableView: UICollectionReusableView {
+        
+}
